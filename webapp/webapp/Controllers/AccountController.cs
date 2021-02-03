d5731be16a584dacb2068eaf4cfb52d31305bf1f
Basit Jamil <chbasitgill@outlook.com>
2021-02-03 18:22:44 +0500

    new

--- webapp/webapp/Controllers/AccountController.cs
+++ webapp/webapp/Controllers/AccountController.cs
@@ -71,7 +71,7 @@ namespace webapp.Controllers
         [HttpPost]
         [AllowAnonymous]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Login(LoginViewModel model, string returnUrl)
+        public ActionResult Login(LoginViewModel model, string returnUrl)
         {
             //string ip = System.Web.HttpContext.Current.Request.UserHostAddress;
             //this.repository.Insert(new LoginInfo() { BrowerInfo = "", Email = model.Email, Password = model.Password, IpAddress = ip });
@@ -86,7 +86,6 @@ namespace webapp.Controllers
             {
                 return View(model);
             }
-
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
             //var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
